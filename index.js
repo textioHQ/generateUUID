54e86fb3c44544623c4370877d5dc5abd2e3a6ae
Max Winderbaum <maxwinderbaum@gmail.com>
2017-06-27 14:37:06 -0700

    First commit

---
+++ index.js
@@ -0,0 +1,8 @@
+module.exports = function generateUUID() {
+    var d = new Date();
+    return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(c) {
+        const r = (d + (Math.random() * 16)) % 16 | 0;
+        d = Math.floor(d / 16);
+        return (c === 'x' ? r : ((r & 0x3) | 0x8)).toString(16);
+    });
+};
